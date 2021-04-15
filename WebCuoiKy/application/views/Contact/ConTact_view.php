8c5ba656cea9c23cc50d2b2af05ce40a131bfba3
Yukiko <72648727+Ninpuhurri@users.noreply.github.com>
2021-04-15 12:43:27 +0700

    Update ConTact_view.php

--- WebCuoiKy/application/views/Contact/ConTact_view.php
+++ WebCuoiKy/application/views/Contact/ConTact_view.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 <!-- menu -->
-<nav  class="navbar navbar-expand-lg navbar-light bg-light" >
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
 	  <a class="navbar-brand" href="#">GGWP SHOP</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
@@ -21,26 +21,53 @@
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto">
 	      <li class="nav-item active">
-	        <a class="nav-link" href="<?php echo base_url() ?>">TRANG CHU <span class="sr-only">(current)</span></a>
+	        <a class="nav-link" href="<?php echo base_url() ?>">TRANG CHỦ <span class="sr-only">(current)</span></a>
 	      </li>
 	      <li class="nav-item">
-	        <a class="nav-link" href="#">LIEN HE</a>
+	        <a class="nav-link" href="<?php echo base_url() ?>index.php/ConTactPage">LIÊN HỆ</a>
 	      </li>
 	      <li class="nav-item dropdown">
 	        <a class="nav-link" href="#" id="navbarDropdown" >
-	          SAN PHAM
+	          SẢN PHẨM
 	        </a>
 	        <div class="dropdown-content">
-	          <a class="dropdown-item" href="#">AO THUN</a>
-	          <a class="dropdown-item" href="#">AO KHOAC</a>
-	          <a class="dropdown-item" href="#">AO SO MI</a>
+	          <?php foreach ($arLoaiao as $key => $value): ?>
+	          	 <a class="dropdown-item" href="<?php echo base_url()?>index.php/HomePage/IDLOAIAO/<?php echo $value['idloaiao'] ?>"><?php echo $value['tenloaiao'] ?></a>
+	          <?php endforeach ?>
 	        </div>
 	      </li>
 	    </ul>
-	    <form class="form-inline my-2 my-lg-0">
-	      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-	      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">TIM KIEM</button>
-	    </form>
+	    <!-- btn Gio Hang -->
+		<a href="<?php echo base_url() ?>index.php/CartPage" class="btn btn-info" role="button">Giỏ Hàng
+			<span class="badge badge-light">
+				<?php if ($this->session->userdata('accout')): ?>
+
+					<?php 
+					$data=$this->session->all_userdata();
+					if(isset($data['soluongsanpham']))
+					{
+						echo $data['soluongsanpham'];
+					}
+					else
+					{
+						echo 0;
+					}
+					?>
+			<?php endif ?>
+			</span>
+		</a>
+<!-- 		btnLOGIN -->
+		<?php if ($this->session->userdata('accout')&&$this->session->userdata('accout')['tentaikhoan']=="a"): ?>
+			<a href="<?php echo base_url() ?>index.php/SignInPage" class="btn btn-info" role="button">Đăng Nhập</a>
+
+		<?php endif ?>
+		<?php if ($this->session->userdata('accout')): ?>
+				<?php if ($this->session->userdata('accout')['tentaikhoan']!="a"): ?>
+					<a href="<?php echo base_url() ?>index.php/HomePage/LogOut" class="btn btn-info" role="button">Đăng Xuất</a>
+					<span>Hi, <?php echo $this->session->userdata('accout')['tenhienthi'] ?>
+					</span>
+				<?php endif ?>
+			<?php endif ?>
 	  </div>
 </nav>
 <!-- end menu -->

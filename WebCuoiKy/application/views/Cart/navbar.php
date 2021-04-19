65db9b3d41419ed2bb2272fc8a72a53c573f4aec
Yukiko <72648727+Ninpuhurri@users.noreply.github.com>
2021-04-19 16:05:48 +0700

    Update navbar.php

--- WebCuoiKy/application/views/Cart/navbar.php
+++ WebCuoiKy/application/views/Cart/navbar.php
@@ -4,7 +4,6 @@
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
-
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto">
 	      <li class="nav-item active">
@@ -25,8 +24,8 @@
 	      </li>
 	    </ul>
 <!-- 	    btn search -->
-	    <form class="form-inline my-2 my-lg-0">
-	      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+	    <form class="form-inline my-2 my-lg-0" method="get" action="<?php echo base_url() ?>index.php/HomePage/search_keyword">
+	      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="keyword">
 	      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
 	    </form>
 	    <!-- btn Gio Hang -->
